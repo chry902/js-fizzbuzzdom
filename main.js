0088d956899674a8261605bd520e666a1d76b0a5
chry902 <tchry902@gmail.com>
2021-10-13 17:51:07 +0200

    avanzamento

--- main.js
+++ main.js
@@ -7,13 +7,15 @@ for (let i = 0; i <= 100; i++) {
     const element = `<div class= "box box--${i}">${i}</div>`;
     section.insertAdjacentHTML(`beforeend`, element);
 
-}
 
 
-section.classList.add(`.section`);
+
+    section.classList.add(`section`);
 
 
-if (i % 3 == 0) {
-    box.classList.add(`.per3`);
+    if (i % 3 == 0 && i % 5 == 0) {
+        box.classList.add(`entrambi3_5`);
+
+    }
 
 }
